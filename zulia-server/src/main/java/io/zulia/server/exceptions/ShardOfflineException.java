c92b78948e0319fda2f5e221e5fe80c7cc91ca18
Matt Davis <matt.davis@ascend-tech.us>
2017-08-11 18:47:42 -0400

    more fun under the sun

---
+++ zulia-server/src/main/java/io/zulia/server/exceptions/ShardOfflineException.java
@@ -0,0 +1,32 @@
+package io.zulia.server.exceptions;
+
+import io.zulia.message.ZuliaBase.MasterSlaveSettings;
+
+import java.io.IOException;
+
+public class ShardOfflineException extends IOException {
+
+	private static final long serialVersionUID = 1L;
+	private final int shardNumber;
+	private final MasterSlaveSettings masterSlaveSettings;
+	private String indexName;
+
+	public ShardOfflineException(String indexName, int shardNumber, MasterSlaveSettings masterSlaveSettings) {
+		super("Index <" + indexName + "> with shardNumber + <" + shardNumber + "> is offline for <" + masterSlaveSettings + ">");
+		this.indexName = indexName;
+		this.shardNumber = shardNumber;
+		this.masterSlaveSettings = masterSlaveSettings;
+	}
+
+	public String getIndexName() {
+		return indexName;
+	}
+
+	public int getShardNumber() {
+		return shardNumber;
+	}
+
+	public MasterSlaveSettings getMasterSlaveSettings() {
+		return masterSlaveSettings;
+	}
+}
