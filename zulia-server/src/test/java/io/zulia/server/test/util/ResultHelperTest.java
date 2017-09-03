5e76e4f0393f8471acea6721a5f15a58b8672401
Matt Davis <matt.davis@ascend-tech.us>
2017-09-03 15:36:47 -0400

    add result helper test case

---
+++ zulia-server/src/test/java/io/zulia/server/test/util/ResultHelperTest.java
@@ -0,0 +1,52 @@
+package io.zulia.server.test.util;
+
+import io.zulia.util.ResultHelper;
+import org.bson.Document;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ResultHelperTest {
+
+	@Test
+	public void testFieldExtraction() throws Exception {
+
+		Document testMongoDocument = new Document();
+		testMongoDocument.put("field1", "someVal");
+		testMongoDocument.put("myfield", 40);
+
+		Document embeddedDocumentOne = new Document();
+		embeddedDocumentOne.put("subfield1", "val2");
+
+		Document embeddedDocumentTwo = new Document();
+		embeddedDocumentTwo.put("otherfield", "1");
+		embeddedDocumentOne.put("subfield2", embeddedDocumentTwo);
+
+		testMongoDocument.put("field2", embeddedDocumentOne);
+
+		List<Document> docs = new ArrayList<>();
+		Document embeddedDocumentThree = new Document();
+		embeddedDocumentThree.put("key1", "val1");
+		embeddedDocumentThree.put("key2", "val2");
+		Document embeddedDocumentFour = new Document();
+		embeddedDocumentFour.put("key1", "someval");
+		docs.add(embeddedDocumentThree);
+		docs.add(embeddedDocumentFour);
+		testMongoDocument.put("thisfield", docs);
+
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "thisfield.key1"), Arrays.asList("val1", "someval"));
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "thisfield.key2"), Arrays.asList("val2"));
+
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "field2.subfield2.otherfield"), "1");
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "field2.subfield2.otherfield1"), null);
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "field2.subfield1.otherfield"), null);
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "thing"), null);
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "field1"), "someVal");
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "field2.subfield1"), "val2");
+		Assert.assertEquals(ResultHelper.getValueFromMongoDocument(testMongoDocument, "myfield"), 40);
+
+	}
+}
