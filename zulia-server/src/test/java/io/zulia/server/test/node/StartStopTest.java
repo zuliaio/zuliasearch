7080663d7df16f5268688e7f1642d5b649fded74
Matt Davis <matt.davis@ascend-tech.us>
2018-02-22 18:06:48 -0500

    start to code start stop test for zulia

--- zulia-server/src/test/java/io/zulia/server/test/node/StartStopTest.java
+++ zulia-server/src/test/java/io/zulia/server/test/node/StartStopTest.java
@@ -1,6 +1,26 @@
 package io.zulia.server.test.node;
 
+import io.zulia.DefaultAnalyzers;
+import io.zulia.client.command.Query;
+import io.zulia.client.command.Store;
+import io.zulia.client.config.ClientIndexConfig;
+import io.zulia.client.pool.ZuliaWorkPool;
+import io.zulia.client.result.QueryResult;
+import io.zulia.doc.ResultDocBuilder;
+import io.zulia.fields.FieldConfigBuilder;
+import io.zulia.message.ZuliaIndex.FacetAs.DateHandling;
+import io.zulia.message.ZuliaIndex.FieldConfig.FieldType;
+import io.zulia.message.ZuliaQuery.FacetCount;
+import org.bson.Document;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class StartStopTest {
 
@@ -12,14 +32,183 @@ public class StartStopTest {
 	private final String[] issns = new String[] { "1234-1234", "3333-1234", "1234-5555", "1234-4444", "2222-2222" };
 	private int totalRecords = COUNT_PER_ISSN * issns.length;
 
-	public static void main(String[] args) throws Exception {
-		TestHelper.startNodes(3);
+	private ZuliaWorkPool zuliaWorkPool;
 
-		TestHelper.stopNodes();
+	@Test
+	public void init() throws Exception {
+
+		TestHelper.createNodes(3);
+
+		TestHelper.startNodes();
+
+		zuliaWorkPool = TestHelper.createClient();
+
+		ClientIndexConfig indexConfig = new ClientIndexConfig();
+		indexConfig.addDefaultSearchField("title");
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("title", FieldType.STRING).indexAs(DefaultAnalyzers.STANDARD));
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("issn", FieldType.STRING).indexAs(DefaultAnalyzers.LC_KEYWORD).facet());
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("eissn", FieldType.STRING).indexAs(DefaultAnalyzers.LC_KEYWORD));
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("uid", FieldType.STRING).indexAs(DefaultAnalyzers.LC_KEYWORD));
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("an", FieldType.NUMERIC_INT).index());
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("country", FieldType.STRING).indexAs(DefaultAnalyzers.LC_KEYWORD).facet());
+		indexConfig.addFieldConfig(FieldConfigBuilder.create("date", FieldType.DATE).index().facetAs(DateHandling.DATE_YYYY_MM_DD));
+		indexConfig.setIndexName(FACET_TEST_INDEX);
+		indexConfig.setNumberOfShards(1);
+
+		zuliaWorkPool.createIndex(indexConfig);
 	}
 
-	@Test(dependsOnMethods="someBloodyMethod")
-	public void testFieldExtraction() throws Exception {
+	@Test(dependsOnMethods = "init")
+	public void index() throws Exception {
+		int id = 0;
+		{
+			for (String issn : issns) {
+				for (int i = 0; i < COUNT_PER_ISSN; i++) {
+					boolean half = (i % 2 == 0);
+					boolean tenth = (i % 10 == 0);
 
+					id++;
+
+					String uniqueId = uniqueIdPrefix + id;
+
+					Document mongoDocument = new Document();
+					mongoDocument.put("issn", issn);
+					mongoDocument.put("title", "Facet Userguide");
+
+					if (half) { // 1/2 of input
+						mongoDocument.put("country", "US");
+					}
+					else { // 1/2 of input
+						mongoDocument.put("country", "France");
+					}
+
+					if (tenth) { // 1/10 of input
+
+						Date d = Date.from(LocalDate.of(2014, Month.OCTOBER, 4).atStartOfDay(ZoneId.of("UTC")).toInstant());
+						mongoDocument.put("date", d);
+					}
+					else if (half) { // 2/5 of input
+						Date d = Date.from(LocalDate.of(2013, Month.SEPTEMBER, 4).atStartOfDay(ZoneId.of("UTC")).toInstant());
+						mongoDocument.put("date", d);
+					}
+					else { // 1/2 of input
+						Date d = Date.from(LocalDate.of(2013, 8, 4).atStartOfDay(ZoneId.of("UTC")).toInstant());
+						mongoDocument.put("date", d);
+					}
+
+					Store s = new Store(uniqueId, FACET_TEST_INDEX);
+					s.setResultDocument(ResultDocBuilder.newBuilder().setDocument(mongoDocument));
+
+					zuliaWorkPool.store(s);
+				}
+			}
+		}
+	}
+
+	@Test(dependsOnMethods = "index")
+	public void restart() throws Exception {
+		TestHelper.stopNodes();
+		Thread.sleep(20000);
+		TestHelper.startNodes();
+	}
+
+	@Test(dependsOnMethods = "restart")
+	public void confirm() throws Exception {
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addCountRequest("issn", 30);
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			Assert.assertEquals(qr.getTotalHits(), totalRecords, "Total record count not " + totalRecords);
+
+			assertEquals("Total facets not " + issns.length, qr.getFacetCounts("issn").size(), issns.length);
+			for (FacetCount fc : qr.getFacetCounts("issn")) {
+				System.out.println(fc.getFacet() + ": " + fc.getCount());
+				assertEquals("Count for facet <" + fc.getFacet() + "> not <" + COUNT_PER_ISSN + ">", COUNT_PER_ISSN, fc.getCount());
+			}
+
+		}
+
+		{
+
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addCountRequest("date", 30);
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count not " + totalRecords, totalRecords, qr.getTotalHits());
+
+			assertEquals("Total facets not " + 3, 3, qr.getFacetCounts("date").size());
+			for (@SuppressWarnings("unused") FacetCount fc : qr.getFacetCounts("date")) {
+				//System.out.println(fc);
+			}
+
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("date", "2014-10-04");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + totalRecords / 10, totalRecords / 10, qr.getTotalHits());
+
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("date", "2013-09-04");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + (totalRecords * 2) / 5, (totalRecords * 2) / 5, qr.getTotalHits());
+
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("date", "2013-08-04");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + totalRecords / 2, totalRecords / 2, qr.getTotalHits());
+
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("issn", "1234-1234");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + COUNT_PER_ISSN, COUNT_PER_ISSN, qr.getTotalHits());
+
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("issn", "1234-1234").addDrillDown("issn", "3333-1234");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + (COUNT_PER_ISSN * 2), COUNT_PER_ISSN * 2, qr.getTotalHits());
+		}
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10).addDrillDown("issn", "1234-1234").addDrillDown("country", "France");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + (COUNT_PER_ISSN / 2), COUNT_PER_ISSN / 2, qr.getTotalHits());
+			assertEquals("Total record count after drill down not " + (COUNT_PER_ISSN / 2), COUNT_PER_ISSN / 2, qr.getTotalHits());
+		}
+
+		{
+			Query q = new Query(FACET_TEST_INDEX, "title:userguide", 10);
+			q.addDrillDown("issn", "1234-1234").addDrillDown("country", "France");
+			q.addCountRequest("issn");
+
+			QueryResult qr = zuliaWorkPool.query(q);
+
+			assertEquals("Total record count after drill down not " + (COUNT_PER_ISSN / 2), COUNT_PER_ISSN / 2, qr.getTotalHits());
+			assertEquals("Number of issn facets not equal " + 1, 1, qr.getFacetCounts("issn").size());
+		}
+	}
+
+	@Test(dependsOnMethods = "confirm")
+	public void shutdown() throws Exception {
+		TestHelper.stopNodes();
+		zuliaWorkPool.shutdown();
 	}
 }
