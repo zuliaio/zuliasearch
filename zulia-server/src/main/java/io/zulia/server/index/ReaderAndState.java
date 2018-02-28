023838b4cd33ccf3f2c1f25e88fe0150d124db48
Matt Davis <matt.davis@ascend-tech.us>
2018-02-28 12:04:31 -0500

    remove query cache from reader and state

--- zulia-server/src/main/java/io/zulia/server/index/ReaderAndState.java
+++ zulia-server/src/main/java/io/zulia/server/index/ReaderAndState.java
@@ -1,6 +1,5 @@
 package io.zulia.server.index;
 
-import io.zulia.server.search.QueryResultCache;
 import org.apache.lucene.facet.sortedset.DefaultSortedSetDocValuesReaderState;
 import org.apache.lucene.facet.sortedset.SortedSetDocValuesReaderState;
 import org.apache.lucene.index.DirectoryReader;
@@ -11,11 +10,9 @@ public class ReaderAndState implements AutoCloseable {
 
 	private DirectoryReader reader;
 	private SortedSetDocValuesReaderState sortedSetDocValuesReaderState;
-	private QueryResultCache qrc;
 
 	public ReaderAndState(DirectoryReader reader) throws IOException {
 		this.reader = reader;
-		this.qrc = new QueryResultCache();
 		try {
 			this.sortedSetDocValuesReaderState = new DefaultSortedSetDocValuesReaderState(reader);
 		}
