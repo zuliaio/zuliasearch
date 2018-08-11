22dae3f86ec648ef45e739a73ba3c93702b54ef8
Matt Davis <matt.davis@ascend-tech.us>
2018-08-11 13:37:54 -0400

    #7 add protobuf-java-util back in after grpc dropped it fix a deprecation warming on maxMessageSize, now maxInboundMessageSize

--- zulia-server/src/main/java/io/zulia/server/connection/server/ZuliaServiceServer.java
+++ zulia-server/src/main/java/io/zulia/server/connection/server/ZuliaServiceServer.java
@@ -1,12 +1,11 @@
 package io.zulia.server.connection.server;
 
 import io.grpc.Server;
-import io.grpc.netty.NettyServerBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import io.zulia.server.config.ZuliaConfig;
 import io.zulia.server.index.ZuliaIndexManager;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 
 /**
  * Created by Matt Davis on 6/28/17.
@@ -16,12 +15,12 @@ public class ZuliaServiceServer {
 
 	private Server server;
 
-	public ZuliaServiceServer(ZuliaConfig zuliaConfig, ZuliaIndexManager indexManager) throws UnknownHostException {
+	public ZuliaServiceServer(ZuliaConfig zuliaConfig, ZuliaIndexManager indexManager) {
 
 		int externalServicePort = zuliaConfig.getServicePort();
 
 		ZuliaServiceHandler zuliaServiceHandler = new ZuliaServiceHandler(indexManager);
-		server = NettyServerBuilder.forPort(externalServicePort).addService(zuliaServiceHandler).maxMessageSize(128 * 1024 * 1024).build();
+		server = NettyServerBuilder.forPort(externalServicePort).addService(zuliaServiceHandler).maxInboundMessageSize(128 * 1024 * 1024).build();
 	}
 
 	public void start() throws IOException {
