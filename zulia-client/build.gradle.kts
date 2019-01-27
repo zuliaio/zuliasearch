a886670c25efe2c47fccddcc5ffb7f592761d8f2
Matt Davis <mdavis95@users.noreply.github.com>
2019-01-26 21:04:30 -0500

    Gradle Kotlin DSL (#10)
    * port to kotlin dsl
    * update testng to the latest 6.x

---
+++ zulia-client/build.gradle.kts
@@ -0,0 +1,6 @@
+description = "Zulia Client"
+
+dependencies {
+    compile(project(":zulia-common"))
+}
+
---
+++ zulia-common/build.gradle.kts
@@ -0,0 +1,55 @@
+import com.google.protobuf.gradle.*
+plugins {
+    id("com.google.protobuf")
+}
+
+
+
+defaultTasks("generateProto", "build")
+
+description = "Zulia Common"
+
+val grpcVersion = "1.16.1"
+
+dependencies {
+    compile("io.grpc:grpc-netty-shaded:${grpcVersion}")
+    compile("io.grpc:grpc-protobuf:${grpcVersion}")
+    compile("io.grpc:grpc-stub:${grpcVersion}")
+    compile("org.mongodb:bson:3.9.0")
+    compile("org.apache.commons:commons-pool2:2.6.0")
+    compile("com.google.protobuf:protobuf-java:3.6.1")
+    compile("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+
+
+protobuf {
+
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc")
+                //"enable_deprecated=false"
+            }
+        }
+    }
+
+    generatedFilesBaseDir = "$projectDir/gen"
+}
+
+tasks {
+    clean {
+        delete("$projectDir/gen")
+    }
+
+}
+
+
