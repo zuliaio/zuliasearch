7f7dc1be11b9ab209cebb0700d8e9f98c0f5fc08
Matthew Davis <matt.davis@ascend-tech.us>
2020-01-08 18:06:04 -0500

    add array length and char length indexing

--- zulia-server/src/main/java/io/zulia/server/index/field/FieldIndexer.java
+++ zulia-server/src/main/java/io/zulia/server/index/field/FieldIndexer.java
@@ -1,7 +1,11 @@
 package io.zulia.server.index.field;
 
+import io.zulia.ZuliaConstants;
 import io.zulia.util.ZuliaUtil;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class FieldIndexer {
 
@@ -11,14 +15,20 @@ public abstract class FieldIndexer {
 
 	public void index(Document document, String storedFieldName, Object storedValue, String indexedFieldName) throws Exception {
 
+
+		AtomicInteger listSize = new AtomicInteger(-1);
 		ZuliaUtil.handleLists(storedValue, obj -> {
 			try {
 				handleValue(document, storedFieldName, obj, indexedFieldName);
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
-		});
+		}, listSize);
+
+		//if stored value is a list or array
+		if (listSize.get() != -1) {
+			document.add(new IntPoint(ZuliaConstants.LIST_LENGTH_PREFIX + indexedFieldName, listSize.get()));
+		}
 
 	}
 
