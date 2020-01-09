92e16f5e633119d519c441bbd8224edabb355102
Matt Davis <matt.davis@ascend-tech.us>
2020-01-09 15:20:40 -0500

    switch to junit 5.5 from test ng

--- zulia-client/src/test/java/io/zulia/client/MapperTest.java
+++ zulia-client/src/test/java/io/zulia/client/MapperTest.java
@@ -2,8 +2,8 @@ package io.zulia.client;
 
 import io.zulia.fields.Mapper;
 import org.bson.Document;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,18 +22,18 @@ public class MapperTest {
 
 		Document doc = mapper.toDocument(testObj1);
 
-		Assert.assertEquals(doc.getString("field1"), "test");
-		Assert.assertEquals((int) doc.getInteger("field2"), 14);
-		Assert.assertEquals(doc.getList("field3", String.class).size(), 2);
-		Assert.assertEquals(((Collection<Integer>) doc.get("field4")).size(), 3);
-		Assert.assertEquals(d, doc.getDate("field5"));
+		Assertions.assertEquals("test", doc.getString("field1"));
+		Assertions.assertEquals(14, (int) doc.getInteger("field2"));
+		Assertions.assertEquals(2, doc.getList("field3", String.class).size());
+		Assertions.assertEquals(3, ((Collection<Integer>) doc.get("field4")).size());
+		Assertions.assertEquals(doc.getDate("field5"), d);
 
 		TestObj1 testObj1a = mapper.fromDocument(doc);
-		Assert.assertEquals(testObj1.getField1(), testObj1a.getField1());
-		Assert.assertEquals(testObj1.getField2(), testObj1a.getField2());
-		Assert.assertEquals(testObj1.getField3(), testObj1a.getField3());
-		Assert.assertEquals(testObj1.getField4(), testObj1a.getField4());
-		Assert.assertEquals(testObj1.getField5(), testObj1a.getField5());
+		Assertions.assertEquals(testObj1a.getField1(), testObj1.getField1());
+		Assertions.assertEquals(testObj1a.getField2(), testObj1.getField2());
+		Assertions.assertEquals(testObj1a.getField3(), testObj1.getField3());
+		Assertions.assertEquals(testObj1a.getField4(), testObj1.getField4());
+		Assertions.assertEquals(testObj1a.getField5(), testObj1.getField5());
 
 		System.out.println(doc);
 
