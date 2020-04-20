b1c1dcf4707e04a691f73623c5ba7f5dc283c8a2
Payam Meyer <payam.meyer@ascend-tech.us>
2020-04-20 09:46:54 -0400

    Added a long and boolean test too.

--- zulia-client/src/test/java/io/zulia/client/MapperTest.java
+++ zulia-client/src/test/java/io/zulia/client/MapperTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Date;
 import java.util.Set;
 
@@ -16,7 +15,7 @@ public class MapperTest {
 	public void testSimpleCase() throws Exception {
 
 		Date d = new Date();
-		TestObj1 testObj1 = new TestObj1("test", 14, Arrays.asList("1", "2"), Set.of(4, 6, 7), d);
+		TestObj1 testObj1 = new TestObj1("test", 14, Arrays.asList("1", "2"), Set.of(4, 6, 7), d, Long.MIN_VALUE, true);
 
 		Mapper<TestObj1> mapper = new Mapper<>(TestObj1.class);
 
@@ -25,8 +24,10 @@ public class MapperTest {
 		Assertions.assertEquals("test", doc.getString("field1"));
 		Assertions.assertEquals(14, (int) doc.getInteger("field2"));
 		Assertions.assertEquals(2, doc.getList("field3", String.class).size());
-		Assertions.assertEquals(3, ((Collection<Integer>) doc.get("field4")).size());
+		Assertions.assertEquals(3, doc.getList("field4", Integer.class).size());
 		Assertions.assertEquals(doc.getDate("field5"), d);
+		Assertions.assertEquals(Long.MIN_VALUE, doc.getLong("field6"));
+		Assertions.assertEquals(true, doc.getBoolean("field7"));
 
 		TestObj1 testObj1a = mapper.fromDocument(doc);
 		Assertions.assertEquals(testObj1a.getField1(), testObj1.getField1());
@@ -34,6 +35,8 @@ public class MapperTest {
 		Assertions.assertEquals(testObj1a.getField3(), testObj1.getField3());
 		Assertions.assertEquals(testObj1a.getField4(), testObj1.getField4());
 		Assertions.assertEquals(testObj1a.getField5(), testObj1.getField5());
+		Assertions.assertEquals(testObj1a.getField6(), testObj1.getField6());
+		Assertions.assertEquals(testObj1a.isField7(), testObj1.isField7());
 
 		System.out.println(doc);
 
