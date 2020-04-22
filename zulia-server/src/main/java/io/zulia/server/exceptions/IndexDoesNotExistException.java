fd6b2a1f080f78ae059ad18721c893312f3a4d30
Matt Davis <matt.davis@ascend-tech.us>
2020-04-22 14:21:58 -0400

    in progress index alias

--- zulia-server/src/main/java/io/zulia/server/exceptions/IndexDoesNotExistException.java
+++ zulia-server/src/main/java/io/zulia/server/exceptions/IndexDoesNotExistException.java
@@ -2,12 +2,12 @@ package io.zulia.server.exceptions;
 
 import java.io.IOException;
 
-public class IndexDoesNotExist extends IOException {
+public class IndexDoesNotExistException extends IOException {
 
 	private static final long serialVersionUID = 1L;
 	private String indexName;
 
-	public IndexDoesNotExist(String indexName) {
+	public IndexDoesNotExistException(String indexName) {
 		super("Index <" + indexName + "> does not exist");
 		this.indexName = indexName;
 	}
