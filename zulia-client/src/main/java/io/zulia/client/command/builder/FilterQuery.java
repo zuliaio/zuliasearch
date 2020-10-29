2e5d45ca779eff907f08a15e786f4f156985b8be
Matthew Davis <matt.davis@ascend-tech.us>
2020-10-29 09:59:11 -0400

    add FILTER_NOT and TERMS_NOT query type on server add .exclude() and .include() to TermQuery And FilterQuery

--- zulia-client/src/main/java/io/zulia/client/command/builder/FilterQuery.java
+++ zulia-client/src/main/java/io/zulia/client/command/builder/FilterQuery.java
@@ -3,13 +3,31 @@ package io.zulia.client.command.builder;
 import io.zulia.message.ZuliaQuery;
 
 public class FilterQuery extends StandardQuery {
+
+	private boolean exclude = false;
+
 	public FilterQuery(String query) {
 		super(query);
 	}
 
+	public FilterQuery include() {
+		exclude = false;
+		return this;
+	}
+
+	public FilterQuery exclude() {
+		exclude = true;
+		return this;
+	}
+
 	@Override
 	protected void completeQuery(ZuliaQuery.Query.Builder queryBuilder) {
-		queryBuilder.setQueryType(ZuliaQuery.Query.QueryType.FILTER);
+		if (exclude) {
+			queryBuilder.setQueryType(ZuliaQuery.Query.QueryType.FILTER_NOT);
+		}
+		else {
+			queryBuilder.setQueryType(ZuliaQuery.Query.QueryType.FILTER);
+		}
 	}
 
 }
