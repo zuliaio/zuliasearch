4bbfdd5b6b874f19937b6c93f3b72d3279824c45
Matthew Davis <matt.davis@ascend-tech.us>
2021-06-05 09:46:11 -0400

    add basic idea behind storage of files

--- zulia-server/src/main/java/io/zulia/server/filestorage/FileDocumentStorage.java
+++ zulia-server/src/main/java/io/zulia/server/filestorage/FileDocumentStorage.java
@@ -9,19 +9,32 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class FileDocumentStorage implements DocumentStorage {
 	@SuppressWarnings("unused")
 	private final static Logger log = Logger.getLogger(FileDocumentStorage.class.getSimpleName());
+	private final String indexName;
+	private final String filesPath;
 
 	public FileDocumentStorage(ZuliaConfig zuliaConfig, String indexName) {
-
+		this.indexName = indexName;
+		this.filesPath = zuliaConfig.getDataPath() + File.separator + "files";
 	}
 
 	@Override
 	public void storeAssociatedDocument(AssociatedDocument docs) throws Exception {
+		String pathToDocument = createPathIfNotExists(docs.getDocumentUniqueId(), docs.getFilename());
+		Files.write(Path.of(pathToDocument, docs.getFilename()), docs.getDocument().toByteArray());
+		//TODO document metadata / timestamp
+	}
+
+	@Override
+	public void storeAssociatedDocument(String uniqueId, String fileName, InputStream is, long timestamp, Document metadataMap) throws Exception {
+		String pathToDocument = createPathIfNotExists(uniqueId, fileName);
 		throw new RuntimeException("Not implemented");
 	}
 
@@ -40,10 +53,6 @@ public class FileDocumentStorage implements DocumentStorage {
 		throw new RuntimeException("Not implemented");
 	}
 
-	@Override
-	public void storeAssociatedDocument(String uniqueId, String fileName, InputStream is, long timestamp, Document metadataMap) throws Exception {
-		throw new RuntimeException("Not implemented");
-	}
 
 	@Override
 	public InputStream getAssociatedDocumentStream(String uniqueId, String filename) {
@@ -75,16 +84,37 @@ public class FileDocumentStorage implements DocumentStorage {
 		throw new RuntimeException("Not implemented");
 	}
 
+	private String createPathIfNotExists(String uniqueId, String fileName) throws Exception {
+		String pathToDocument = getFullPathToDocument(uniqueId);
+		File f = new File(pathToDocument);
+		if (!f.exists()) {
+			boolean created = f.mkdirs();
+			if (!created) {
+				if (!f.exists()) {
+					throw new Exception(
+							"Failed to create directory for associated document <" + fileName + "> for unique id <" + uniqueId + "> for indexName <" + indexName
+									+ "> in path <" + pathToDocument + ">");
+				}
+				if (!f.isDirectory()) {
+					throw new Exception(
+							"Failed to create directory for associated document <" + fileName + "> for unique id <" + uniqueId + "> for indexName <" + indexName
+									+ "> in path <" + pathToDocument + "> because path exist and is not a directory");
+				}
+			}
+		}
+		return pathToDocument;
+	}
+
+	private String getFullPathToDocument(String uniqueId) {
+		return filesPath + File.separator + indexName + File.separator + getPathToDocument(uniqueId);
+	}
+
 	private static String getPathToDocument(String uniqueId) {
 		String hexHash = Integer.toHexString(uniqueId.hashCode());
 		String piece1 = hexHash.substring(0, 2);
 		String piece2 = hexHash.substring(2, 5);
 		String piece3 = hexHash.substring(5);
-
 		return piece1 + File.separator + piece2 + File.separator + piece3;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(getPathToDocument("someDoc"));
-	}
 }
