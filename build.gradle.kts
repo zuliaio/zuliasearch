c8cf704a93b94e17f0292fca4b262caa3237cded
Matthew Davis <matt.davis@ascend-tech.us>
2021-09-09 07:37:37 -0400

    remove maven central publishing and configure maven-publishing for gradle 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,70 +46,47 @@ subprojects {
     artifacts.add("archives", sourcesJar)
     artifacts.add("archives", javadocJar)
 
-    if (project.hasProperty("sonatypeUsername")) {
-
-        publishing {
-            publications {
-                create<MavenPublication>("mavenJava") {
-                    from(components["java"])
-                    artifact(tasks["sourcesJar"])
-                    artifact(tasks["javadocJar"])
-                    pom {
-                        url.set("http://zulia.io")
-                        licenses {
-                            license {
-                                name.set("The Apache License, Version 2.0")
-                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set("mdavis")
-                                name.set("Matt Davis")
-                                email.set("matt.davis@ascend-tech.us")
-                                organization.set("Ascendant Software Technology, LLC")
-                                organizationUrl.set("http://www.ascend-tech.us")
-                            }
-                            developer {
-                                id.set("payam.meyer")
-                                name.set("Payam Mayer")
-                                email.set("payam.meyer@ascend-tech.us")
-                                organization.set("Ascendant Software Technology, LLC")
-                                organizationUrl.set("http://www.ascend-tech.us")
-                            }
-                        }
-                        scm {
-                            connection.set("git@github.com:zuliaio/zuliasearch.git")
-                            developerConnection.set("git@github.com:zuliaio/zuliasearch.git")
-                            url.set("https://github.com/zuliaio/zuliasearch")
-                        }
-                        name.set(project.name)
-                        description.set(project.name)
-                    }
-                }
-                repositories {
-                    maven {
-                        val sonatypeUsername: String by project
-                        val sonatypePassword: String by project
-
-                        val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                        val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
-                        url = uri(if (!project.version.toString().contains("rc")) releasesRepoUrl else snapshotsRepoUrl)
-                        credentials {
-                            username = sonatypeUsername
-                            password = sonatypePassword
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifact(tasks["sourcesJar"])
+                artifact(tasks["javadocJar"])
+                pom {
+                    url.set("http://zulia.io")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                         }
                     }
+                    developers {
+                        developer {
+                            id.set("mdavis")
+                            name.set("Matt Davis")
+                            email.set("matt.davis@ascend-tech.us")
+                            organization.set("Ascendant Software Technology, LLC")
+                            organizationUrl.set("http://www.ascend-tech.us")
+                        }
+                        developer {
+                            id.set("payam.meyer")
+                            name.set("Payam Mayer")
+                            email.set("payam.meyer@ascend-tech.us")
+                            organization.set("Ascendant Software Technology, LLC")
+                            organizationUrl.set("http://www.ascend-tech.us")
+                        }
+                    }
+                    scm {
+                        connection.set("git@github.com:zuliaio/zuliasearch.git")
+                        developerConnection.set("git@github.com:zuliaio/zuliasearch.git")
+                        url.set("https://github.com/zuliaio/zuliasearch")
+                    }
+                    name.set(project.name)
+                    description.set(project.name)
                 }
             }
-
-        }
-
-        signing {
-            sign(publishing.publications["mavenJava"])
         }
     }
-
 
     group = "io.zulia"
 
