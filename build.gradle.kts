0be32dc54ff9a0bd2275be40bfc003bd0dbaa2d4
Matthew Davis <matt.davis@ascend-tech.us>
2021-11-08 08:00:33 -0500

    bump java level to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ subprojects {
     apply(plugin = "maven-publish")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     val sourcesJar = tasks.register<Jar>("sourcesJar") {
