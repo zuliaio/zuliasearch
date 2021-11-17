ceff471e93562f7454cacab8b1edff27cae9a18f
Matthew Davis <matt.davis@ascend-tech.us>
2021-11-17 15:41:00 -0500

    refactor query combiner into more methods / pieces try to optimize the one shard and one index case a little

--- zulia-server/src/main/java/io/zulia/server/search/QueryCombiner.java
+++ zulia-server/src/main/java/io/zulia/server/search/QueryCombiner.java
@@ -1,9 +1,7 @@
 package io.zulia.server.search;
 
-import io.zulia.ZuliaConstants;
 import io.zulia.message.ZuliaBase.Term;
 import io.zulia.message.ZuliaIndex.FieldConfig;
-import io.zulia.message.ZuliaQuery;
 import io.zulia.message.ZuliaQuery.AnalysisRequest;
 import io.zulia.message.ZuliaQuery.AnalysisResult;
 import io.zulia.message.ZuliaQuery.CountRequest;
@@ -15,14 +13,11 @@ import io.zulia.message.ZuliaQuery.LastResult;
 import io.zulia.message.ZuliaQuery.ScoredResult;
 import io.zulia.message.ZuliaQuery.ShardQueryResponse;
 import io.zulia.message.ZuliaQuery.SortRequest;
-import io.zulia.message.ZuliaQuery.SortValues;
 import io.zulia.message.ZuliaServiceOuterClass.InternalQueryResponse;
 import io.zulia.message.ZuliaServiceOuterClass.QueryRequest;
 import io.zulia.message.ZuliaServiceOuterClass.QueryResponse;
 import io.zulia.server.analysis.frequency.TermFreq;
-import io.zulia.server.field.FieldTypeUtil;
 import io.zulia.server.index.ZuliaIndex;
-import org.apache.lucene.util.BytesRef;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,29 +31,23 @@ import java.util.logging.Logger;
 
 public class QueryCombiner {
 
+	private final static Logger log = Logger.getLogger(QueryCombiner.class.getSimpleName());
+
 	private final static Comparator<ScoredResult> scoreCompare = new ScoreCompare();
 	private final static Comparator<ScoredResult> reverseScoreCompare = new ReverseScoreCompare();
 
-	private final static Logger log = Logger.getLogger(QueryCombiner.class.getSimpleName());
-
 	private final List<InternalQueryResponse> responses;
-
 	private final Map<String, Map<Integer, ShardQueryResponse>> indexToShardQueryResponseMap;
 	private final List<ShardQueryResponse> shardResponses;
-
 	private final int amount;
 	private final int start;
 	private final LastResult lastResult;
 	private final List<AnalysisRequest> analysisRequestList;
-
-	private boolean isShort;
-	private List<ScoredResult> results;
-	private int resultsSize;
-
-	private SortRequest sortRequest;
-
+	private final SortRequest sortRequest;
 	private final Collection<ZuliaIndex> indexes;
 	private final Map<String, Integer> indexToShardCount;
+
+	private boolean isShort;
 
 	public QueryCombiner(Collection<ZuliaIndex> indexes, QueryRequest request, List<InternalQueryResponse> responses) {
 		this.indexToShardCount = new HashMap<>();
@@ -79,8 +68,7 @@ public class QueryCombiner {
 		this.analysisRequestList = request.getAnalysisRequestList();
 
 		this.isShort = false;
-		this.results = Collections.emptyList();
-		this.resultsSize = 0;
+
 	}
 
 	private void validate() throws Exception {
@@ -134,8 +122,6 @@ public class QueryCombiner {
 
 		validate();
 
-		boolean sorting = (sortRequest != null && !sortRequest.getFieldSortList().isEmpty());
-
 		long totalHits = 0;
 		long returnedHits = 0;
 		for (ShardQueryResponse sr : shardResponses) {
@@ -146,24 +132,7 @@ public class QueryCombiner {
 		QueryResponse.Builder builder = QueryResponse.newBuilder();
 		builder.setTotalHits(totalHits);
 
-		resultsSize = Math.min(amount, (int) returnedHits);
-
-		results = Collections.emptyList();
-
-		Map<String, ScoredResult[]> lastIndexResultMap = new HashMap<>();
-
-		for (String indexName : indexToShardQueryResponseMap.keySet()) {
-			int numberOfShards = indexToShardCount.get(indexName);
-			lastIndexResultMap.put(indexName, new ScoredResult[numberOfShards]);
-		}
-
-		for (LastIndexResult lir : lastResult.getLastIndexResultList()) {
-			ScoredResult[] lastForShardArr = lastIndexResultMap.get(lir.getIndexName());
-			// initialize with last results
-			for (ScoredResult sr : lir.getLastForShardList()) {
-				lastForShardArr[sr.getShard()] = sr;
-			}
-		}
+		int resultsSize = Math.min(amount, (int) returnedHits);
 
 		Map<CountRequest, FacetCombiner> facetCombinerMap = new HashMap<>();
 
@@ -189,19 +158,14 @@ public class QueryCombiner {
 				Map<String, Term.Builder> termMap = analysisRequestToTermMap.get(analysisRequest);
 
 				for (Term term : analysisResult.getTermsList()) {
-
 					String key = term.getValue();
 					if (!termMap.containsKey(key)) {
 						termMap.put(key, Term.newBuilder().setValue(key).setDocFreq(0).setTermFreq(0));
 					}
 					Term.Builder termsBuilder = termMap.get(key);
-
 					termsBuilder.setDocFreq(termsBuilder.getDocFreq() + term.getDocFreq());
-
 					termsBuilder.setScore(termsBuilder.getScore() + term.getScore());
-
 					termsBuilder.setTermFreq(termsBuilder.getTermFreq() + term.getTermFreq());
-
 				}
 			}
 
@@ -223,17 +187,141 @@ public class QueryCombiner {
 			builder.addFacetGroup(facetCombiner.getCombinedFacetGroup());
 		}
 
-		List<ScoredResult> mergedResults = new ArrayList<>((int) returnedHits);
+		Map<String, ScoredResult[]> lastIndexResultMap = createLastIndexResultMapWithPreviousLastResults();
+		List<ScoredResult> results;
+		if (shardResponses.size() > 1) {
+			results = mergeResults((int) returnedHits, resultsSize, lastIndexResultMap);
+		}
+		else {
+			ShardQueryResponse shardQueryResponse = shardResponses.get(0);
+			results = shardQueryResponse.getScoredResultList();
+			if (!results.isEmpty()) {
+				lastIndexResultMap.get(shardQueryResponse.getIndexName())[shardQueryResponse.getShardNumber()] = results.get(results.size() - 1);
+			}
+		}
+
+		if (start == 0) {
+			builder.addAllResults(results);
+		}
+		else {
+			int i = 0;
+			for (ScoredResult scoredResult : results) {
+				if (i >= start) {
+					builder.addResults(scoredResult);
+				}
+				i++;
+			}
+		}
+
+		builder.setLastResult(createLastResult(lastIndexResultMap));
+
+		return builder.build();
+	}
+
+	private List<ScoredResult> mergeResults(int returnedHits, int resultsSize, Map<String, ScoredResult[]> lastIndexResultMap) throws Exception {
+
+		List<ScoredResult> results = Collections.emptyList();
+
+		boolean sorting = (sortRequest != null && !sortRequest.getFieldSortList().isEmpty());
+
+		List<ScoredResult> mergedResults = new ArrayList<>(returnedHits);
 		for (ShardQueryResponse sr : shardResponses) {
 			mergedResults.addAll(sr.getScoredResultList());
 		}
 
-		Comparator<ScoredResult> myCompare = scoreCompare;
+		if (!mergedResults.isEmpty()) {
 
-		if (sorting) {
-			final List<FieldSort> fieldSortList = sortRequest.getFieldSortList();
+			List<FieldSort> fieldSortList = sortRequest != null ? sortRequest.getFieldSortList() : Collections.emptyList();
+			HashMap<String, FieldConfig.FieldType> sortTypeMap = createSortTypeMap(fieldSortList);
 
-			final HashMap<String, FieldConfig.FieldType> sortTypeMap = new HashMap<>();
+			Comparator<ScoredResult> comparator = new ZuliaPostSortingComparator(fieldSortList, sortTypeMap);
+			mergedResults.sort(comparator);
+
+			results = mergedResults.subList(0, resultsSize);
+
+			for (ScoredResult sr : results) {
+				ScoredResult[] lastForShardArr = lastIndexResultMap.get(sr.getIndexName());
+				lastForShardArr[sr.getShard()] = sr;
+			}
+
+			outside:
+			for (ZuliaIndex index : indexes) {
+				String indexName = index.getIndexName();
+				ScoredResult[] lastForShardArr = lastIndexResultMap.get(indexName);
+				ScoredResult lastForIndex = null;
+				for (ScoredResult sr : lastForShardArr) {
+					if (sr != null) {
+						if (lastForIndex == null) {
+							lastForIndex = sr;
+						}
+						else {
+							if (comparator.compare(sr, lastForIndex) > 0) {
+								lastForIndex = sr;
+							}
+						}
+					}
+				}
+
+				if (lastForIndex == null) {
+					//this happen when amount from index is zero
+					continue;
+				}
+
+				double shardTolerance = index.getShardTolerance();
+
+				int numberOfShards = index.getNumberOfShards();
+				Map<Integer, ShardQueryResponse> shardResponseMap = indexToShardQueryResponseMap.get(indexName);
+				for (int shardNumber = 0; shardNumber < numberOfShards; shardNumber++) {
+					ShardQueryResponse sr = shardResponseMap.get(shardNumber);
+					if (sr.hasNext()) {
+						ScoredResult next = sr.getNext();
+						int compare = comparator.compare(lastForIndex, next);
+						if (compare > 0) {
+
+							if (sorting) {
+								String msg = "Result set did not return the most relevant sorted documents for index <" + indexName + ">\n";
+								msg += "    Last for index from shard <" + lastForIndex.getShard() + "> has sort values <" + lastForIndex.getSortValues() + ">\n";
+								msg += "    Next for shard <" + next.getShard() + ">  has sort values <" + next.getSortValues() + ">\n";
+								msg += "    Last for shards: \n";
+								msg += "      " + Arrays.toString(lastForShardArr) + "\n";
+								msg += "    Results: \n";
+								msg += "      " + results + "\n";
+								msg += "    If this happens frequently increase requestFactor or minShardRequest\n";
+								msg += "    Retrying with full request.\n";
+								log.severe(msg);
+
+								isShort = true;
+								break outside;
+							}
+
+							double diff = (Math.abs(lastForIndex.getScore() - next.getScore()));
+							if (diff > shardTolerance) {
+								String msg = "Result set did not return the most relevant documents for index <" + indexName + "> with shard tolerance <" + shardTolerance + ">\n";
+								msg += "    Last for index from shard <" + lastForIndex.getShard() + "> has score <" + lastForIndex.getScore() + ">\n";
+								msg += "    Next for shard <" + next.getShard() + "> has score <" + next.getScore() + ">\n";
+								msg += "    Last for shards: \n";
+								msg += "      " + Arrays.toString(lastForShardArr) + "\n";
+								msg += "    Results: \n";
+								msg += "      " + results + "\n";
+								msg += "    If this happens frequently increase requestFactor, minShardRequest, or shardTolerance\n";
+								msg += "    Retrying with full request.\n";
+								log.severe(msg);
+
+								isShort = true;
+								break outside;
+							}
+						}
+					}
+				}
+			}
+
+		}
+		return results;
+	}
+
+	private HashMap<String, FieldConfig.FieldType> createSortTypeMap(List<FieldSort> fieldSortList) throws Exception {
+		HashMap<String, FieldConfig.FieldType> sortTypeMap = new HashMap<>();
+		if (!fieldSortList.isEmpty()) {
 
 			for (FieldSort fieldSort : fieldSortList) {
 				String sortField = fieldSort.getSortField();
@@ -260,217 +348,11 @@ public class QueryCombiner {
 					}
 				}
 			}
-
-			myCompare = (o1, o2) -> {
-				int compare = 0;
-
-				int sortValueIndex = 0;
-
-				SortValues sortValues1 = o1.getSortValues();
-				SortValues sortValues2 = o2.getSortValues();
-				for (FieldSort fs : fieldSortList) {
-					String sortField = fs.getSortField();
-
-					FieldConfig.FieldType sortType = sortTypeMap.get(sortField);
-
-					if (!ZuliaQueryParser.rewriteLengthFields(sortField).equals(sortField)) {
-						sortType = FieldConfig.FieldType.NUMERIC_LONG;
-					}
-
-					if (ZuliaConstants.SCORE_FIELD.equals(sortField)) {
-						if (FieldSort.Direction.DESCENDING.equals(fs.getDirection())) {
-							compare = scoreCompare.compare(o1, o2);
-						}
-						else {
-							compare = reverseScoreCompare.compare(o1, o2);
-						}
-					}
-					else {
-						ZuliaQuery.SortValue sortValue1 = sortValues1.getSortValue(sortValueIndex);
-						ZuliaQuery.SortValue sortValue2 = sortValues2.getSortValue(sortValueIndex);
-
-						if (FieldTypeUtil.isNumericIntFieldType(sortType)) {
-							Integer a = sortValue1.getExists() ? sortValue1.getIntegerValue() : null;
-							Integer b = sortValue2.getExists() ? sortValue2.getIntegerValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(Integer::compareTo).compare(a, b);
-							}
-							else {
-								compare = Comparator.nullsLast(Integer::compareTo).compare(a, b);
-							}
-						}
-						else if (FieldTypeUtil.isNumericLongFieldType(sortType)) {
-							Long a = sortValue1.getExists() ? sortValue1.getLongValue() : null;
-							Long b = sortValue2.getExists() ? sortValue2.getLongValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(Long::compareTo).compare(a, b);
-							}
-							else {
-								compare = Comparator.nullsLast(Long::compareTo).compare(a, b);
-							}
-						}
-						else if (FieldTypeUtil.isDateFieldType(sortType)) {
-							Long a = sortValue1.getExists() ? sortValue1.getDateValue() : null;
-							Long b = sortValue2.getExists() ? sortValue2.getDateValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(Long::compareTo).compare(a, b);
-							}
-							else {
-								compare = Comparator.nullsLast(Long::compareTo).compare(a, b);
-							}
-						}
-						else if (FieldTypeUtil.isNumericFloatFieldType(sortType)) {
-
-							Float a = sortValue1.getExists() ? sortValue1.getFloatValue() : null;
-							Float b = sortValue2.getExists() ? sortValue2.getFloatValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(Float::compareTo).compare(a, b);
-							}
-							else {
-								compare = Comparator.nullsLast(Float::compareTo).compare(a, b);
-							}
-						}
-						else if (FieldTypeUtil.isNumericDoubleFieldType(sortType)) {
-
-							Double a = sortValue1.getExists() ? sortValue1.getDoubleValue() : null;
-							Double b = sortValue2.getExists() ? sortValue2.getDoubleValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(Double::compareTo).compare(a, b);
-							}
-							else {
-								compare = Comparator.nullsLast(Double::compareTo).compare(a, b);
-							}
-
-						}
-						else {
-							String a = sortValue1.getExists() ? sortValue1.getStringValue() : null;
-							String b = sortValue2.getExists() ? sortValue2.getStringValue() : null;
-
-							if (!fs.getMissingLast()) {
-								compare = Comparator.nullsFirst(BytesRef::compareTo)
-										.compare(a != null ? new BytesRef(a) : null, b != null ? new BytesRef(b) : null);
-							}
-							else {
-								compare = Comparator.nullsLast(BytesRef::compareTo)
-										.compare(a != null ? new BytesRef(a) : null, b != null ? new BytesRef(b) : null);
-							}
-						}
-
-						if (FieldSort.Direction.DESCENDING.equals(fs.getDirection())) {
-							compare *= -1;
-						}
-					}
-
-					if (compare != 0) {
-						return compare;
-					}
-
-					sortValueIndex++;
-
-				}
-
-				return compare;
-			};
 		}
+		return sortTypeMap;
+	}
 
-		if (!mergedResults.isEmpty()) {
-			mergedResults.sort(myCompare);
-
-			results = mergedResults.subList(0, resultsSize);
-
-			for (ScoredResult sr : results) {
-				ScoredResult[] lastForShardArr = lastIndexResultMap.get(sr.getIndexName());
-				lastForShardArr[sr.getShard()] = sr;
-			}
-
-			outside:
-			for (ZuliaIndex index : indexes) {
-				String indexName = index.getIndexName();
-				ScoredResult[] lastForShardArr = lastIndexResultMap.get(indexName);
-				ScoredResult lastForIndex = null;
-				for (ScoredResult sr : lastForShardArr) {
-					if (sr != null) {
-						if (lastForIndex == null) {
-							lastForIndex = sr;
-						}
-						else {
-							if (myCompare.compare(sr, lastForIndex) > 0) {
-								lastForIndex = sr;
-							}
-						}
-					}
-				}
-
-				if (lastForIndex == null) {
-					//this happen when amount from index is zero
-					continue;
-				}
-
-				double shardTolerance = index.getShardTolerance();
-
-				int numberOfShards = index.getNumberOfShards();
-				Map<Integer, ShardQueryResponse> shardResponseMap = indexToShardQueryResponseMap.get(indexName);
-				for (int shardNumber = 0; shardNumber < numberOfShards; shardNumber++) {
-					ShardQueryResponse sr = shardResponseMap.get(shardNumber);
-					if (sr.hasNext()) {
-						ScoredResult next = sr.getNext();
-						int compare = myCompare.compare(lastForIndex, next);
-						if (compare > 0) {
-
-							if (sorting) {
-								String msg = "Result set did not return the most relevant sorted documents for index <" + indexName + ">\n";
-								msg += "    Last for index from shard <" + lastForIndex.getShard() + "> has sort values <" + lastForIndex.getSortValues()
-										+ ">\n";
-								msg += "    Next for shard <" + next.getShard() + ">  has sort values <" + next.getSortValues() + ">\n";
-								msg += "    Last for shards: \n";
-								msg += "      " + Arrays.toString(lastForShardArr) + "\n";
-								msg += "    Results: \n";
-								msg += "      " + results + "\n";
-								msg += "    If this happens frequently increase requestFactor or minShardRequest\n";
-								msg += "    Retrying with full request.\n";
-								log.severe(msg);
-
-								isShort = true;
-								break outside;
-							}
-
-							double diff = (Math.abs(lastForIndex.getScore() - next.getScore()));
-							if (diff > shardTolerance) {
-								String msg = "Result set did not return the most relevant documents for index <" + indexName + "> with shard tolerance <"
-										+ shardTolerance + ">\n";
-								msg += "    Last for index from shard <" + lastForIndex.getShard() + "> has score <" + lastForIndex.getScore() + ">\n";
-								msg += "    Next for shard <" + next.getShard() + "> has score <" + next.getScore() + ">\n";
-								msg += "    Last for shards: \n";
-								msg += "      " + Arrays.toString(lastForShardArr) + "\n";
-								msg += "    Results: \n";
-								msg += "      " + results + "\n";
-								msg += "    If this happens frequently increase requestFactor, minShardRequest, or shardTolerance\n";
-								msg += "    Retrying with full request.\n";
-								log.severe(msg);
-
-								isShort = true;
-								break outside;
-							}
-						}
-					}
-				}
-			}
-
-		}
-
-		int i = 0;
-		for (ScoredResult scoredResult : results) {
-			if (i >= start) {
-				builder.addResults(scoredResult);
-			}
-			i++;
-		}
-
+	private LastResult createLastResult(Map<String, ScoredResult[]> lastIndexResultMap) {
 		LastResult.Builder newLastResultBuilder = LastResult.newBuilder();
 		for (String indexName : lastIndexResultMap.keySet()) {
 			ScoredResult[] lastForShardArr = lastIndexResultMap.get(indexName);
@@ -478,8 +360,8 @@ public class QueryCombiner {
 			List<ScoredResult> indexList = new ArrayList<>();
 			for (int shard = 0; shard < numberOfShards; shard++) {
 				if (lastForShardArr[shard] != null) {
-					ScoredResult.Builder minimalSR = ScoredResult.newBuilder(lastForShardArr[shard]);
-					minimalSR = minimalSR.clearUniqueId().clearIndexName().clearResultIndex().clearTimestamp().clearResultDocument();
+					ScoredResult.Builder minimalSR = ScoredResult.newBuilder(lastForShardArr[shard]).clearUniqueId().clearIndexName().clearResultIndex()
+							.clearTimestamp().clearResultDocument();
 					indexList.add(minimalSR.build());
 				}
 			}
@@ -488,10 +370,25 @@ public class QueryCombiner {
 				newLastResultBuilder.addLastIndexResult(lastIndexResult);
 			}
 		}
+		return newLastResultBuilder.build();
+	}
 
-		builder.setLastResult(newLastResultBuilder.build());
+	private Map<String, ScoredResult[]> createLastIndexResultMapWithPreviousLastResults() {
+		Map<String, ScoredResult[]> lastIndexResultMap = new HashMap<>();
 
-		return builder.build();
+		for (String indexName : indexToShardQueryResponseMap.keySet()) {
+			int numberOfShards = indexToShardCount.get(indexName);
+			lastIndexResultMap.put(indexName, new ScoredResult[numberOfShards]);
+		}
+
+		for (LastIndexResult lir : lastResult.getLastIndexResultList()) {
+			ScoredResult[] lastForShardArr = lastIndexResultMap.get(lir.getIndexName());
+			// initialize with last results
+			for (ScoredResult sr : lir.getLastForShardList()) {
+				lastForShardArr[sr.getShard()] = sr;
+			}
+		}
+		return lastIndexResultMap;
 	}
 
 	public boolean isShort() {
