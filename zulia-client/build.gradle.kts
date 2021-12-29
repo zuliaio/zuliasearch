5b2c3e774fa524b5bfb03dc9309d84b7d2549789
Payam Meyer <payam.meyer@ascend-tech.us>
2021-12-29 09:17:58 -0500

    Downgraded Micronaut back to 3.0.0 as the issue is still prevelant even with the client forced. Fixed a NoSuchFileException for when associated files are not extractable.

--- zulia-client/build.gradle.kts
+++ zulia-client/build.gradle.kts
@@ -9,7 +9,7 @@ val micronautVersion: String by project
 dependencies {
     api(project(":zulia-common"))
     implementation("com.google.code.gson:gson:2.8.6")
-    implementation("io.micronaut:micronaut-http-client:3.0.0") // forcing to working 3.0 until we switch to native java http client.
+    implementation("io.micronaut:micronaut-http-client:$micronautVersion") // forcing to working 3.0 until we switch to native java http client.
     implementation("io.micronaut.reactor:micronaut-reactor-http-client:2.0.0")
 }
 
