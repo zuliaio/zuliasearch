2d1f9b9457579c207ad645df8fdc2df0db978ff7
Matthew Davis <matt.davis@ascend-tech.us>
2022-04-29 19:19:54 -0400

    4 threads by default

--- zulia-server/src/main/java/io/zulia/server/cmd/common/ThreadedArgs.java
+++ zulia-server/src/main/java/io/zulia/server/cmd/common/ThreadedArgs.java
@@ -5,7 +5,7 @@ import picocli.CommandLine;
 public class ThreadedArgs {
 
 	@CommandLine.Option(names = { "--threads", }, description = "Number of threads to use", scope = CommandLine.ScopeType.INHERIT)
-	private int threads = 1;
+	private int threads = 4;
 
 	public int getThreads() {
 		return threads;
