2d79ee23688ceeaab39dd16c2d52e6fff8554890
Matthew Davis <matt.davis@ascend-tech.us>
2022-04-29 14:08:11 -0400

    remove aliases option from displayAliases because it cant be required

--- zulia-server/src/main/java/io/zulia/server/cmd/zuliaadmin/DisplayAliasesCmd.java
+++ zulia-server/src/main/java/io/zulia/server/cmd/zuliaadmin/DisplayAliasesCmd.java
@@ -4,11 +4,9 @@ import io.zulia.client.pool.ZuliaWorkPool;
 import io.zulia.message.ZuliaIndex;
 import io.zulia.server.cmd.ZuliaAdmin;
 import io.zulia.server.cmd.ZuliaCommonCmd;
-import io.zulia.server.cmd.common.AliasArgs;
 import picocli.CommandLine;
 
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "displayAliases", description = "Displays aliases")
@@ -16,9 +14,6 @@ public class DisplayAliasesCmd implements Callable<Integer> {
 
 	@CommandLine.ParentCommand
 	private ZuliaAdmin zuliaAdmin;
-
-	@CommandLine.Mixin
-	private AliasArgs aliasArgs;
 
 	@Override
 	public Integer call() throws Exception {
@@ -29,12 +24,8 @@ public class DisplayAliasesCmd implements Callable<Integer> {
 
 		List<ZuliaIndex.IndexAlias> indexAliases = zuliaWorkPool.getNodes().getIndexAliases();
 
-		Set<String> aliases = aliasArgs.resolveAliases(zuliaWorkPool);
-
 		for (ZuliaIndex.IndexAlias indexAlias : indexAliases) {
-			if (aliases.contains(indexAlias.getAliasName())) {
-				System.out.printf("%40s | %40s", indexAlias.getAliasName(), indexAlias.getIndexName());
-			}
+			System.out.printf("%40s | %40s", indexAlias.getAliasName(), indexAlias.getIndexName());
 		}
 
 		return CommandLine.ExitCode.OK;
