8b7f222bcbf29fbbccfacb3c0c1ee42cb5f778a8
Matthew Davis <matt.davis@ascend-tech.us>
2022-07-07 09:45:16 -0400

    add store and fetch file to zulia admin

--- zulia-server/src/main/java/io/zulia/server/cmd/ZuliaAdmin.java
+++ zulia-server/src/main/java/io/zulia/server/cmd/ZuliaAdmin.java
@@ -3,25 +3,13 @@ package io.zulia.server.cmd;
 import io.zulia.client.pool.ZuliaWorkPool;
 import io.zulia.server.cmd.common.ShowStackArgs;
 import io.zulia.server.cmd.common.ZuliaVersionProvider;
-import io.zulia.server.cmd.zuliaadmin.ClearIndexCmd;
-import io.zulia.server.cmd.zuliaadmin.ConnectionInfo;
-import io.zulia.server.cmd.zuliaadmin.CreateAliasCmd;
-import io.zulia.server.cmd.zuliaadmin.DeleteAliasCmd;
-import io.zulia.server.cmd.zuliaadmin.DeleteIndexCmd;
-import io.zulia.server.cmd.zuliaadmin.DisplayAliasesCmd;
-import io.zulia.server.cmd.zuliaadmin.DisplayIndexesCmd;
-import io.zulia.server.cmd.zuliaadmin.DisplayNodesCmd;
-import io.zulia.server.cmd.zuliaadmin.DocCountCmd;
-import io.zulia.server.cmd.zuliaadmin.ExportAliasesCmd;
-import io.zulia.server.cmd.zuliaadmin.ImportAliasesCmd;
-import io.zulia.server.cmd.zuliaadmin.OptimizeIndexCmd;
-import io.zulia.server.cmd.zuliaadmin.ReindexCmd;
+import io.zulia.server.cmd.zuliaadmin.*;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "zuliaadmin", subcommands = { DisplayNodesCmd.class, DisplayIndexesCmd.class, DocCountCmd.class, ClearIndexCmd.class,
 		DeleteIndexCmd.class, OptimizeIndexCmd.class, ReindexCmd.class, CreateAliasCmd.class, DeleteAliasCmd.class, DisplayAliasesCmd.class,
 		ExportAliasesCmd.class,
-		ImportAliasesCmd.class, }, mixinStandardHelpOptions = true, versionProvider = ZuliaVersionProvider.class, scope = CommandLine.ScopeType.INHERIT)
+		ImportAliasesCmd.class, StoreAssociatedFileCmd.class, FetchAssociatedFileCmd.class }, mixinStandardHelpOptions = true, versionProvider = ZuliaVersionProvider.class, scope = CommandLine.ScopeType.INHERIT)
 public class ZuliaAdmin {
 
 	@CommandLine.Mixin
