4cc2f3d058932ace8417bc5b67439abaee9cd882
Matthew Davis <matt.davis@ascend-tech.us>
2022-11-09 13:35:54 -0500

    improve index settings display on REST to show warmed searches add ability to see if search response is pinned change log line to show if search is returned from the cache

--- zulia-server/src/main/java/io/zulia/server/rest/controllers/IndexController.java
+++ zulia-server/src/main/java/io/zulia/server/rest/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package io.zulia.server.rest.controllers;
 
 import com.cedarsoftware.util.io.JsonWriter;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.util.JsonFormat;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
@@ -9,17 +10,22 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.QueryValue;
 import io.zulia.ZuliaConstants;
+import io.zulia.message.ZuliaIndex;
 import io.zulia.message.ZuliaServiceOuterClass;
 import io.zulia.message.ZuliaServiceOuterClass.GetIndexSettingsResponse;
 import io.zulia.server.index.ZuliaIndexManager;
 import io.zulia.server.util.ZuliaNodeProvider;
 
+import java.util.logging.Logger;
+
 /**
  * Created by Payam Meyer on 8/7/17.
+ *
  * @author pmeyer
  */
 @Controller(ZuliaConstants.INDEX_URL)
 public class IndexController {
+	private final static Logger LOG = Logger.getLogger(IndexController.class.getSimpleName());
 
 	@Get
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -31,14 +37,25 @@ public class IndexController {
 		try {
 			StringBuilder responseBuilder = new StringBuilder();
 
-			GetIndexSettingsResponse getIndexSettingsResponse = indexManager
-					.getIndexSettings(ZuliaServiceOuterClass.GetIndexSettingsRequest.newBuilder().setIndexName(index).build());
+			GetIndexSettingsResponse getIndexSettingsResponse = indexManager.getIndexSettings(
+					ZuliaServiceOuterClass.GetIndexSettingsRequest.newBuilder().setIndexName(index).build());
 
-			responseBuilder.append("{");
-			responseBuilder.append("\"indexSettings\": ");
-			JsonFormat.Printer printer = JsonFormat.printer();
-			responseBuilder.append(printer.print(getIndexSettingsResponse.getIndexSettings()));
-			responseBuilder.append("}");
+			ZuliaIndex.IndexSettings indexSettings = getIndexSettingsResponse.getIndexSettings();
+
+			ZuliaServiceOuterClass.RestIndexSettingsResponse.Builder restIndexSettings = ZuliaServiceOuterClass.RestIndexSettingsResponse.newBuilder()
+					.setIndexSettings(indexSettings);
+
+			for (ByteString bytes : indexSettings.getWarmingSearchesList()) {
+				try {
+					ZuliaServiceOuterClass.QueryRequest queryRequest = ZuliaServiceOuterClass.QueryRequest.parseFrom(bytes);
+					restIndexSettings.addWarmingSearch(queryRequest);
+				}
+				catch (Exception e) {
+					LOG.severe("Failed to parse warming search: " + e.getMessage());
+				}
+			}
+
+			responseBuilder.append(JsonFormat.printer().print(restIndexSettings));
 
 			String docString = responseBuilder.toString();
 
