269cfcda0fb606ae0039c4e28bb628d2bc0568d6
Matthew Davis <matt.davis@ascend-tech.us>
2023-01-31 15:05:02 -0500

    missed lucene 9.5 deprecation issues

--- zulia-server/src/main/java/io/zulia/server/index/ShardDocumentIndexer.java
+++ zulia-server/src/main/java/io/zulia/server/index/ShardDocumentIndexer.java
@@ -19,7 +19,7 @@ import io.zulia.util.ZuliaUtil;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.KnnVectorField;
+import org.apache.lucene.document.KnnFloatVectorField;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.SortedNumericDocValuesField;
 import org.apache.lucene.document.SortedSetDocValuesField;
@@ -120,7 +120,7 @@ public class ShardDocumentIndexer {
 			}
 			else if (FieldTypeUtil.isVectorFieldType(fieldType)) {
 				if (o instanceof Collection collection) {
-					luceneDocument.add(new KnnVectorField(indexedFieldName, Floats.toArray(collection),
+					luceneDocument.add(new KnnFloatVectorField(indexedFieldName, Floats.toArray(collection),
 							ZuliaIndex.FieldConfig.FieldType.UNIT_VECTOR.equals(fieldType) ?
 									VectorSimilarityFunction.DOT_PRODUCT :
 									VectorSimilarityFunction.COSINE));
