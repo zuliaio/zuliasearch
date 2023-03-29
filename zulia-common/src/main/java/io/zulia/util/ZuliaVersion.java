31307f4338503329a363a5e6c4ca0e2e2eec6b28
Payam Meyer <payam.meyer@ascend-tech.us>
2023-03-29 19:49:40 -0400

    I'm not really seeing the version file this is supposed to read and it's failing hard which is breaking the build, I'm temporarily making it a soft fail.

--- zulia-common/src/main/java/io/zulia/util/ZuliaVersion.java
+++ zulia-common/src/main/java/io/zulia/util/ZuliaVersion.java
@@ -3,19 +3,21 @@ package io.zulia.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ZuliaVersion {
 
+	private static final Logger LOG = Logger.getLogger(ZuliaVersion.class.getSimpleName());
 	private static final ZuliaVersion VERSION = new ZuliaVersion();
-
-	private final String version;
+	private String version;
 
 	public ZuliaVersion() {
 		try (InputStream versionStream = ZuliaVersion.class.getResourceAsStream("/version")) {
 			version = new String(versionStream.readAllBytes(), StandardCharsets.UTF_8);
 		}
 		catch (IOException e) {
-			throw new RuntimeException(e);
+			LOG.log(Level.SEVERE, "Failed to get the version.", e);
 		}
 
 	}
