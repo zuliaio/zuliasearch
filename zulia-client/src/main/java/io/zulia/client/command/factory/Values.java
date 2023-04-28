8983786c7336c7b1f8f6f0ade86b3aa36824d857
Matthew Davis <matt.davis@ascend-tech.us>
2023-04-28 11:18:04 -0400

    add escaping inside of Values and test case

--- zulia-client/src/main/java/io/zulia/client/command/factory/Values.java
+++ zulia-client/src/main/java/io/zulia/client/command/factory/Values.java
@@ -21,14 +21,47 @@ public class Values {
 
 	public static Function<String, String> VALUE_QUOTER = s -> {
 		s = s.trim();
+		//if starts and ends with quotes pass it through
 		if (s.startsWith("\"") && s.endsWith("\"")) {
 			return s;
 		}
+
+		//if it contains a space, quote and pass it through after escaping quotes
 		if (s.contains(" ") || s.contains("-")) {
-			return "\"" + s + "\"";
+			return "\"" + escapeQuotes(s) + "\"";
 		}
-		return s;
+
+		return escape(s);
+
 	};
+
+	public static String escapeQuotes(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			// These characters are part of the query syntax and must be escaped
+			if (c == '\"') {
+				sb.append('\\');
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	//taken from Lucene QueryParserUtil with @ added
+	public static String escape(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			// These characters are part of the query syntax and must be escaped
+			if (c == '\\' || c == '+' || c == '-' || c == '!' || c == '(' || c == ')' || c == ':' || c == '^' || c == '[' || c == ']' || c == '\"' || c == '{'
+					|| c == '}' || c == '~' || c == '*' || c == '?' || c == '|' || c == '&' || c == '/' || c == '@') {
+				sb.append('\\');
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 
 	private final Type type;
 
