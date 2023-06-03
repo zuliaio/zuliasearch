7f88ccfa4800823555a6a40333f6c0530494dba0
Matthew Davis <matt.davis@ascend-tech.us>
2023-06-03 09:25:49 -0400

    get default command line server and port from command line if they are available closes #110

--- zulia-server/src/main/java/io/zulia/server/cmd/zuliaadmin/ConnectionInfo.java
+++ zulia-server/src/main/java/io/zulia/server/cmd/zuliaadmin/ConnectionInfo.java
@@ -7,10 +7,10 @@ import picocli.CommandLine;
 public class ConnectionInfo {
 
 	@CommandLine.Option(names = { "--address", "--server" }, description = "Zulia Server Address")
-	public String address = "localhost";
+	public String address = System.getenv("ZULIA_HOST") != null ? System.getenv("ZULIA_HOST") : "localhost";
 
 	@CommandLine.Option(names = "--port", description = "Zulia Port")
-	public Integer port = 32191;
+	public Integer port = System.getenv("ZULIA_PORT") != null ? Integer.parseInt(System.getenv("ZULIA_PORT")) : 32191;
 
 	@CommandLine.Option(names = "--routing", description = "Use smart routing to route request to the correct node (do not use with ssh port forwarding)")
 	public boolean routingEnabled;
