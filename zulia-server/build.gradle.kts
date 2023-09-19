3275f731a3a436f5f24ddd199b6c386099d4fb76
Matthew Davis <matt.davis@ascend-tech.us>
2023-09-19 15:01:59 -0400

    bump flapdoodle embedded mongo to 4.9.2 from 4.7.0

--- zulia-server/build.gradle.kts
+++ zulia-server/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     testImplementation(platform("io.micronaut:micronaut-bom:$micronautVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("io.micronaut.test:micronaut-test-junit5")
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.7.0")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
     api("org.yaml:snakeyaml:$snakeYamlVersion")
