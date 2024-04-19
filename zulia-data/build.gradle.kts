446b8c6e17789ef464cbf302351be0303308429c
Matthew Davis <matt.davis@ascend-tech.us>
2024-04-19 10:05:40 -0400

    ensure zulia data has the common compress CVE update when used by itself

--- zulia-data/build.gradle.kts
+++ zulia-data/build.gradle.kts
@@ -7,6 +7,7 @@ description = "Zulia Data"
 
 dependencies {
     api(libs.bundles.poi)
+    api(libs.commons.compress)
     api(libs.univocity.parsers)
     api(libs.jackson.databind)
     api(projects.zuliaUtil)
