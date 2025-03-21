4e0e85631bb6ae6ded989a3f9092c2659ce6b4b2
Matthew Davis <mdavis95@users.noreply.github.com>
2025-03-21 11:51:53 -0400

    Breaking change that requires reindexing for Zulia clusters with shards:  Switch shard hashing to use a better hash function than the built in java hashcode().  Shortcircuit computing the hash if only one shard. (#134)

--- zulia-common/src/main/java/io/zulia/util/ShardUtil.java
+++ zulia-common/src/main/java/io/zulia/util/ShardUtil.java
@@ -2,6 +2,18 @@ package io.zulia.util;
 
 public class ShardUtil {
 	public static int findShardForUniqueId(String uniqueId, int numOfShards) {
-		return Math.abs(uniqueId.hashCode()) % numOfShards;
+		return (int) Math.abs(djb2Hash(uniqueId)) % numOfShards;
 	}
+	
+	public static long djb2Hash(String str) {
+		
+		long hash = 5381;
+		
+		for (int i = 0; i < str.length(); i++) {
+			hash = ((hash << 5) + hash) + str.charAt(i);
+		}
+		
+		return hash;
+	}
+	
 }
