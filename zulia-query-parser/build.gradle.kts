9a5893c5e12dbea65574d5287816f08acc00a1f9
Matt Davis <matt.davis@ascend-tech.us>
2026-01-13 16:00:24 -0500

    Make minimum should match queries have default OR handling in the minimum should match context of parens.  + / - / NoT inside the mm should match block are kept.  At a global level setting minimumNumberShouldMatch on a query sets defaultOperator to OR regardless of what a user set. Setup query parser test cases and first test case for  minimum should match

--- zulia-query-parser/build.gradle.kts
+++ zulia-query-parser/build.gradle.kts
@@ -3,6 +3,9 @@ description = "Zulia Query Parser"
 dependencies {
     api(projects.zuliaAnalyzer)
     api(libs.lucene.queryparser)
+
+    testRuntimeClasspath(libs.logback.classic)
+    testRuntimeClasspath(libs.jansi)
 }
 
 
