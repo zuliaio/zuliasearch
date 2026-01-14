681c1b98b2dbd98dc70d9e49c93ed214e64ddafa
Matthew Davis <mdavis95@users.noreply.github.com>
2026-01-14 10:16:29 -0800

    Make minimum should match queries have default OR handling in the minimum should match context of parens.  + / - / NoT inside the mm should match block are kept.  At a global level setting minimumNumberShouldMatch on a query sets defaultOperator to OR regardless of what a user set. (#153)
    Setup query parser test cases and first test case for  minimum should match

---
+++ zulia-query-parser/src/test/java/io/zulia/queryparser/test/QueryParserTest.java
@@ -0,0 +1,63 @@
+package io.zulia.queryparser.test;
+
+import io.zulia.DefaultAnalyzers;
+import io.zulia.message.ZuliaIndex;
+import io.zulia.message.ZuliaQuery;
+import io.zulia.server.analysis.ZuliaPerFieldAnalyzer;
+import io.zulia.server.config.ServerIndexConfig;
+import io.zulia.server.search.queryparser.ZuliaFlexibleQueryParser;
+import org.apache.lucene.search.Query;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class QueryParserTest {
+
+	@Test
+	public void minimumShouldMatchTest() throws Exception {
+
+		ZuliaIndex.IndexSettings.Builder builder = ZuliaIndex.IndexSettings.newBuilder();
+		builder.addFieldConfig(ZuliaIndex.FieldConfig.newBuilder()
+				.addIndexAs(ZuliaIndex.IndexAs.newBuilder().setAnalyzerName(DefaultAnalyzers.STANDARD).setIndexFieldName("title").build())
+				.setFieldType(ZuliaIndex.FieldConfig.FieldType.STRING).setStoredFieldName("title").build());
+		builder.addFieldConfig(ZuliaIndex.FieldConfig.newBuilder()
+				.addIndexAs(ZuliaIndex.IndexAs.newBuilder().setAnalyzerName(DefaultAnalyzers.STANDARD).setIndexFieldName("abstract").build())
+				.setFieldType(ZuliaIndex.FieldConfig.FieldType.STRING).setStoredFieldName("abstract").build());
+
+		ServerIndexConfig serverIndexConfig = new ServerIndexConfig(builder.build());
+		ZuliaPerFieldAnalyzer zuliaPerFieldAnalyzer = new ZuliaPerFieldAnalyzer(serverIndexConfig);
+		ZuliaFlexibleQueryParser zuliaFlexibleQueryParser = new ZuliaFlexibleQueryParser(zuliaPerFieldAnalyzer, serverIndexConfig);
+
+
+		Query parse;
+
+		String mmQuery = "abstract:diabetes title:(cancer AND lung -fly rat bear insect +fruit)@2";
+
+		zuliaFlexibleQueryParser.setDefaultOperator(ZuliaQuery.Query.Operator.OR);
+		parse = zuliaFlexibleQueryParser.parse(mmQuery);
+		Assertions.assertEquals("abstract:diabetes ((+title:cancer +title:lung -title:fly title:rat title:bear title:insect +title:fruit)~2)",
+				parse.toString());
+
+		zuliaFlexibleQueryParser.setDefaultOperator(ZuliaQuery.Query.Operator.AND);
+		parse = zuliaFlexibleQueryParser.parse(mmQuery);
+		Assertions.assertEquals("+abstract:diabetes +((+title:cancer +title:lung -title:fly title:rat title:bear title:insect +title:fruit)~2)",
+				parse.toString());
+
+		zuliaFlexibleQueryParser.setMinimumNumberShouldMatch(2);
+		zuliaFlexibleQueryParser.setDefaultFields(List.of("title"));
+		String mmQuery2 = "cancer AND lung -fly rat bear insect +fruit dragon";
+
+		zuliaFlexibleQueryParser.setDefaultOperator(ZuliaQuery.Query.Operator.OR);
+		parse = zuliaFlexibleQueryParser.parse(mmQuery2);
+		Assertions.assertEquals("(+title:cancer +title:lung -title:fly title:rat title:bear title:insect +title:fruit title:dragon)~2",
+				parse.toString());
+
+		zuliaFlexibleQueryParser.setDefaultOperator(ZuliaQuery.Query.Operator.AND);
+		parse = zuliaFlexibleQueryParser.parse(mmQuery2);
+		Assertions.assertEquals("(+title:cancer +title:lung -title:fly title:rat title:bear title:insect +title:fruit title:dragon)~2",
+				parse.toString());
+
+	}
+
+}
