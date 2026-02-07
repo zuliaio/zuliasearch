72885dfcb2cbc8b3cb2630617cef70ad1be2b04b
Matt Davis <matt.davis@ascend-tech.us>
2026-02-06 20:21:39 -0500

    upgrade to latest graal js (25.0.2 from 23.0.3, now two dependencies)

--- zulia-testing/build.gradle.kts
+++ zulia-testing/build.gradle.kts
@@ -3,6 +3,7 @@ description = "Zulia Testing"
 dependencies {
     api(projects.zuliaUtil)
     api(projects.zuliaClient)
+    implementation(libs.graalvm.polyglot)
     implementation(libs.graalvm.js)
     implementation(libs.logback.classic)
     implementation(libs.jansi)
