da777957250f68f511b2801c7f23b6b9c8e559db
Matt Davis <matt.davis@ascend-tech.us>
2026-02-12 19:53:46 -0500

    Fix for BatchDeleteResult: The original BatchDeleteResult stored the gRPC streaming Iterator<DeleteResponse> but never consumed it

--- zulia-client/src/main/java/io/zulia/client/result/BatchDeleteResult.java
+++ zulia-client/src/main/java/io/zulia/client/result/BatchDeleteResult.java
@@ -2,15 +2,23 @@ package io.zulia.client.result;
 
 import io.zulia.message.ZuliaServiceOuterClass.DeleteResponse;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BatchDeleteResult extends Result {
 
-	@SuppressWarnings("unused")
-	private final Iterator<DeleteResponse> batchDeleteResponse;
+	private final List<DeleteResponse> deleteResponses;
 
 	public BatchDeleteResult(Iterator<DeleteResponse> batchDeleteResponse) {
-		this.batchDeleteResponse = batchDeleteResponse;
+		this.deleteResponses = new ArrayList<>();
+		while (batchDeleteResponse.hasNext()) {
+			deleteResponses.add(batchDeleteResponse.next());
+		}
+	}
+
+	public int getDeleteCount() {
+		return deleteResponses.size();
 	}
 
 }
