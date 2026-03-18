65b4386e38d7db8ca132935227e2c4402d0b4d37
Matt Davis <matt.davis@ascend-tech.us>
2026-03-18 11:26:01 -0400

    add comment about super rare case that Math.abs does not work for

--- zulia-common/src/main/java/io/zulia/util/ShardUtil.java
+++ zulia-common/src/main/java/io/zulia/util/ShardUtil.java
@@ -3,6 +3,7 @@ package io.zulia.util;
 public class ShardUtil {
 	public static int findShardForUniqueId(String uniqueId, int numOfShards) {
 		long shard = Math.abs(djb2Hash(uniqueId)) % numOfShards;
+		// TODO replace this with Math.floorMod or handle the silly uniqueId == Long.MIN_VALUE case, but these will break existing sharding
 		return (int) shard; // Typecast after modulus to ensure it's within int range
 	}
 
@@ -16,5 +17,6 @@ public class ShardUtil {
 
 		return hash;
 	}
+	
 
 }
