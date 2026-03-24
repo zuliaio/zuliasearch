6319e76840f8568356bc62f14d3e4c2690d31c62
Matt Davis <matt.davis@ascend-tech.us>
2026-03-24 15:18:13 -0400

    add ben-manes.versions to show dependencies that might need updating run with ./gradlew  dependencyUpdates -DoutputFormatter=html --no-parallel --no-configuration-cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,25 @@ plugins {
     idea
     signing
     `maven-publish`
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 
 allprojects {
     group = "io.zulia"
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
 
 
