0fcd8048e0ae1e8c33944691b75f9574e16e7cc9
Matt Davis <matt.davis@ascend-tech.us>
2026-08-23 12:45:19 -0400

    Revert embedded test MongoDB to 7.0, kernel 6.19+ blocks all current 8.x binaries

--- zulia-ui-rest/src/test/java/io/zulia/ui/rest/test/MongoTestInstance.java
+++ zulia-ui-rest/src/test/java/io/zulia/ui/rest/test/MongoTestInstance.java
@@ -35,11 +35,14 @@ public class MongoTestInstance {
 					.build();
 
 			port = de.flapdoodle.commons.net.Net.freeServerPort(de.flapdoodle.commons.net.Net.getLocalHost());
+			// MongoDB 8.x binaries (8.0.23 through 8.3.2) refuse to start on Linux kernel 6.19 and newer because of a
+			// TCMalloc rseq incompatibility (SERVER-121912). The fixed binaries only allow kernel 7.0.14 and above
+			// (SERVER-125742). Stay on 7.0 until the dev and CI kernels are past that line.
 			running = Mongod.builder()
 					.processOutput(Start.to(ProcessOutput.class).initializedWith(processOutput))
 					.net(Start.to(Net.class).initializedWith(Net.defaults().withPort(port)))
 					.build()
-					.start(Version.Main.V8_0);
+					.start(Version.Main.V7_0);
 			testInstanceUrl = LOCAL_INSTANCE_URL_HOST + ":" + port;
 		}
 		catch (Exception ex) {
